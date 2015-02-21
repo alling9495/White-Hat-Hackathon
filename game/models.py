7160ed9c00ecc02e3dc2ad3c2faf770010ce6c28
Liam Kirsh <liam.kirsh@riseup.net>
2015-02-21 14:24:30 -0800

    hello world

--- game/models.py
+++ game/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+'''class Game(models.Model):
+    authors = models.ForeignKey('auth.Users')
+    password = models.CharField(max_length=200)
+
+    def start'''
