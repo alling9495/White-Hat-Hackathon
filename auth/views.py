663d5e53f018f844c763bc630996395e1a5faea9
Liam Kirsh <liam.kirsh@riseup.net>
2015-02-21 16:37:33 -0800

    login works!

--- auth/views.py
+++ auth/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template.defaulttags import csrf_token
+from django.template import RequestContext
 
 # Create your views here.
 # auth/views.py
-from django.shortcuts import render_to_response
 from django.contrib.auth import authenticate, login
 
+#@csrf_token
 def login_user(request):
     state = "Please log in below..."
     username = password = ''
@@ -22,4 +25,4 @@ def login_user(request):
         else:
             state = "Your username and/or password were incorrect."
 
-    return render_to_response('auth.html',{'state':state, 'username': username})
+    return render_to_response('auth.html',{'state':state, 'username': username}, context_instance=RequestContext(request))
